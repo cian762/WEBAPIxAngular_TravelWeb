2ef8b0e8841a44d966b30d710447e2a00ad7042c
FuChen0930 <chenguanfu4@gmail.com>
2026-03-10 18:22:52 +0800

    20260310_ActivityAPI_活動前台篩選功能(20%)

---
+++ backend/TravelWeb-API/TravelWeb-API/DTO/ActivityDTO/FilterActivityDTO.cs
@@ -0,0 +1,12 @@
+﻿using TravelWeb_API.Models.ActivityModel;
+
+namespace TravelWeb_API.DTO.ActivityDTO
+{
+    public class FilterActivityDTO
+    {
+        public string? ActivityType { get; set; }
+        public string? LaunchRegion { get; set; }
+        public DateOnly? StartTime { get; set; }
+        public DateOnly? EndTime { get; set; }
+    }
+}
