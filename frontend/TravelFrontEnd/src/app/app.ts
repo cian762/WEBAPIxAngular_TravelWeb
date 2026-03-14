a72a1a72b025fd37fac49cd95e8f4deb2b7dd596
Frank Chen <chenguanfu4@gmail.com>
2026-03-14 22:26:06 +0800

    20260314 完成 Angular 專案公版建立

--- frontend/TravelFrontEnd/src/app/app.ts
+++ frontend/TravelFrontEnd/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestUse } from "./Components/test-use/test-use";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestUse],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
