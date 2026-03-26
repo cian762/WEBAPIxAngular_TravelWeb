2cec58e7b36a4a3a19a1f51c80280a485ea7b1d4
LeeHungYee <ping79715@gmail.com>
2026-03-26 10:15:38 +0800

    20260326調整banner顯示

--- frontend/TravelFrontEnd/src/app/app.ts
+++ frontend/TravelFrontEnd/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router, NavigationEnd } from '@angular/router';
 import { TestUse } from "./Components/test-use/test-use";
 import { BlogHome } from "./Board/blog-home/blog-home";
 import { Header } from "./header/header";
@@ -11,14 +11,34 @@ import { Order } from "./trip/component/order/order";
 import { Product } from "./trip/component/product/product";
 import { TripProductDetail } from "./trip/component/trip-product-detail/trip-product-detail";
 import { LoginComponent } from "./Member/login/login.component";
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Banner],
+  imports: [RouterOutlet, Header, Footer, Banner, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  showBanner = true;
+  constructor(private router: Router) {
+    // 監聽路由變換
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const currentUrl = event.urlAfterRedirects || event.url;
+
+      // 使用 includes 檢查，只要網址裡面包含這個字眼，就隱藏 Banner
+      // 或者是判斷是不是「首頁」 (/)
+      //const hideRoutes = ['/tripProduct', '/login', '/member'];
+      const hidePath = 'tripProduct';
+
+      // 邏輯：如果目前網址包含 hidePath，就不顯示 (false)
+      this.showBanner = !currentUrl.includes(hidePath);
+
+    });
+  }
   ngOnInit(): void {
     this.initializeGuestId();
   }
@@ -40,4 +60,5 @@ export class App implements OnInit {
       console.log('分配遊客身份：', newGuestId);
     }
   }
+
 }
